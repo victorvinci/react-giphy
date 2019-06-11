4ae7f0c42d75bae7ffae5e51a1e54f01d2acb1d7
victor vinci <victorvinci@protonmail.com>
2019-06-11 21:40:51 +0200

    Updates

---
+++ precache-manifest.c5f5ab8062038851167880baf5dadad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02d2446048736bce45f6ee1f5ef1b69",
+    "url": "/testing-create-react-app/index.html"
+  },
+  {
+    "revision": "f218c56788b487e54872",
+    "url": "/testing-create-react-app/static/css/main.af269a4a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testing-create-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f218c56788b487e54872",
+    "url": "/testing-create-react-app/static/js/main.f3ded1f9.chunk.js"
+  },
+  {
+    "revision": "fe4c7ccfc96dde61b307",
+    "url": "/testing-create-react-app/static/js/runtime~main.5e870016.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
